b02810b610e739d719420ae637f113b1733f20de
Andres Aquino <informatica@alumno.local>
2026-06-19 12:07:21 -0600

    fix(editorial) se guardo la implemntacion del DAO , sin configuracio #22

--- src/org/mkaa/dao/impl/ComprasDaoimpl.java
+++ src/org/mkaa/dao/impl/ComprasDaoimpl.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.mkaa.dao.impl;
 
-/**
- *
- * @author informatica
- */
-public class ComprasDaoimpl {
+import org.key.util.Conexion;
+import org.key.model.Compras;
+        import org.key.dao.Compra;
+
+import java.util.List;
+import java.util.Arraylist;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class ComprasDaoimpl implements ComprasDAO{
+    
+    @Override
+    public boolean insertar(Cliente cliente){
+        return false;
+    }
+    
+    @Override
+    public Cliente buscar(long cui){
+        return null;
+    }
     
 }
