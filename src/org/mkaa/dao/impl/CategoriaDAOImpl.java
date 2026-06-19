1f9135e8a79a08ed5013a7ac43ef446d2415dc8e
Informatica <informatica@alumno.local>
2026-06-19 12:06:34 -0600

    feat(categoria) se agrego la clase POJO para Categoria #20

---
+++ src/org/mkaa/dao/impl/CategoriaDAOImpl.java
@@ -0,0 +1,38 @@
+package org.mkaa.dao.impl;
+
+import org.mkaa.model.Categoria;
+import org.mkaa.dao.impl.CategoriaDAOImpl;
+
+import java.util.List;
+
+public class CategoriaDAOImpl implements CategoriaDAO{
+
+    @Override
+    public boolean insertar(Categoria categoria) {
+        return false; 
+    }
+
+    @Override
+    public List<Categoria> Listar() {
+        return null; 
+    }
+
+    @Override
+    public Categoria buscar(long cui) {
+         return null; 
+    }
+
+    @Override
+    public boolean actualizar(Categoria categoria) {
+        return false; 
+    }
+
+    @Override
+    public boolean eliminar(long cui) {
+        return false; 
+    }
+    }
+    
+    
+    
+
